3655b880c35919ebcc572607d71b667ff6f97002
OzodGulomjonov <102744037+OzodGulomjonov@users.noreply.github.com>
2022-05-07 03:10:06 +0500

    l added site-header to our site

---
+++ script.js
@@ -0,0 +1,14 @@
+const elBox = document.querySelector('.site-header__list')
+    const elBtn = document.querySelector('.site-header__btn')
+
+    elBtn.addEventListener('click', () => {
+      elBox.classList.toggle('site-header__list--active')
+    }) ;
+
+ const elNav = document.querySelector('.site-header__btn')
+    const elButton = document.querySelector('.site-header__btn')
+
+    elButton.addEventListener('click', () => {
+      elBox.classList.toggle('site-header__button--active' )}) 
+     
+
